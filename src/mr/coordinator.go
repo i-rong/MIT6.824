98fb88b75caaea675025ca2e4a9e32d0cb076bf7
rong <1556371277@qq.com>
2023-10-01 21:16:00 +0800

    finish but not test

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,15 +1,52 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"strconv"
+	"strings"
+)
 
+// var mu sync.Locker
 
 type Coordinator struct {
-	// Your definitions here.
+	ReducerNum        int        // 记录Coordinator准备分配几个Reducer
+	TaskId            int        // 记录下一个分配的任务的taskid
+	DistPhase         Phase      // 记录当前分配任务的阶段
+	MapTaskChannel    chan *Task // 存放分配出去的Map task
+	ReduceTaskChannel chan *Task // 存放分配出去的Reduce task
 
+	TaskHolder TaskHolder // 存放所有的task
+}
+
+type Phase int
+
+const (
+	MapPhase Phase = iota
+	ReducePhase
+	Alldone
+)
+
+type TaskStatus int
+
+const (
+	Operating TaskStatus = iota // 表示这个任务正在被某一个worker执行
+	Waiting                     // 表示这个任务等待被某一个worker执行
+	Done                        // 表示这个任务已经被执行完毕
+)
+
+type TaskHolder struct {
+	Meta map[int]*TaskInfo
+}
+
+type TaskInfo struct {
+	TaskStatus TaskStatus // 当前task的状态 有三种 正在执行 等待被执行 执行完毕
+	TaskAddr   *Task      // 当前task的地址
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -23,7 +60,6 @@ func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
-
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -46,12 +82,12 @@ func (c *Coordinator) server() {
 // if the entire job has finished.
 //
 func (c *Coordinator) Done() bool {
-	ret := false
-
-	// Your code here.
-
-
-	return ret
+	if c.DistPhase == Alldone {
+		fmt.Printf("All tasks are finished, the coordinator will exit.\n")
+		return true
+	} else {
+		return false
+	}
 }
 
 //
@@ -60,11 +96,236 @@ func (c *Coordinator) Done() bool {
 // nReduce is the number of reduce tasks to use.
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	c := Coordinator{
+		ReducerNum:        nReduce,
+		TaskId:            0,
+		DistPhase:         MapPhase,
+		MapTaskChannel:    make(chan *Task, len(files)),
+		ReduceTaskChannel: make(chan *Task, nReduce),
 
-	// Your code here.
+		TaskHolder: TaskHolder{
+			Meta: make(map[int]*TaskInfo, len(files)+nReduce), // task的数量是所有map的数量 + reduce的数量
+		},
+	}
 
-
+	c.makeMapTasks(files)
 	c.server()
 	return &c
+}
+
+// 对传进来的files进行处理，将每一个文件名都初始化成一个map task
+func (c *Coordinator) makeMapTasks(files []string) {
+	for _, file := range files {
+		mapTaskId := c.genTaskId() // coordinator分配唯一的任务编号
+		task := Task{
+			TaskType:   MapTask,
+			TaskId:     mapTaskId,
+			ReducerNum: c.ReducerNum,
+			FileNames:  []string{file}, // 一个字符串数组 并将第一个初始化为file
+		}
+
+		c.TaskHolder.AcceptTask(&task)
+		fmt.Printf("The Coordinator makes a map task, which task id is %d\n", task.TaskId)
+		c.MapTaskChannel <- &task
+	}
+}
+
+func (TaskHolder *TaskHolder) AcceptTask(task *Task) bool {
+	taskId := task.TaskId
+	tmp := TaskHolder.Meta[taskId]
+	if tmp != nil { // 说明已经有一个task为taskId的放在TaskHolder里面了
+		fmt.Printf("TaskHolder already has the task %d.\n", taskId)
+		return false
+	} else {
+		taskInfo := TaskInfo{
+			TaskStatus: Waiting, // task的初始状态为等待执行
+			TaskAddr:   task,
+		}
+		TaskHolder.Meta[taskId] = &taskInfo
+		return true
+	}
+}
+
+func (c *Coordinator) genTaskId() int {
+	ret := c.TaskId // 将当前这个数字分配出去
+	c.TaskId++      // 下一个任务的id
+	return ret
+}
+
+// Coordinator分配任务
+func (c *Coordinator) AssignTask(args *ExampleArgs, reply *Task) error {
+	// 分配任务的时候应该上锁 防止多个worker竞争同一个task 并使用defer回退解锁
+	// mu.Lock()
+	// defer mu.Unlock()
+
+	switch c.DistPhase {
+	case MapPhase: // 如果是MapPhase 那么需要给这个worker分配map任务 需要从MapTaskChannel中取出来
+		{
+			if len(c.MapTaskChannel) > 0 {
+				*reply = *<-c.MapTaskChannel // 相当于从队头取出一个task给这个worker
+				taskId := reply.TaskId
+				taskStatus := c.getTaskStatus(taskId)
+				if taskStatus == Operating { // 这个任务正在执行
+					fmt.Printf("The task %d is already running.\n", taskId)
+				} else if taskStatus == Waiting { // 这个任务正在等待被执行
+					c.setTaskStatus(taskId)
+					fmt.Printf("The task %d begins to run.\n", taskId)
+				} else if taskStatus == Done { // 这个任务已经被执行完毕了
+					fmt.Printf("The task %d is done.\n", taskId)
+				}
+			} else { // 没有任务了 说明任务都在被做 或者已经做完了 就把这个worker设置为Waiting
+				reply.TaskType = WaitingTask
+				if c.checkAllTaskDone() {
+					c.toNextPhase()
+				}
+				return nil
+			}
+		}
+	case ReducePhase:
+		{
+			if len(c.ReduceTaskChannel) > 0 {
+				*reply = *<-c.ReduceTaskChannel
+				taskId := reply.TaskId
+				taskStatus := c.getTaskStatus(taskId)
+				if taskStatus == Operating { // 这个任务正在执行
+					fmt.Printf("The task %d is already running.\n", taskId)
+				} else if taskStatus == Waiting { // 这个任务正在等待被执行
+					c.setTaskStatus(taskId)
+					fmt.Printf("The task %d begins to run.\n", taskId)
+				} else if taskStatus == Done { // 这个任务已经被执行完毕了
+					fmt.Printf("The task %d is done.\n", taskId)
+				}
+			} else {
+				reply.TaskType = WaitingTask
+				if c.checkAllTaskDone() {
+					c.toNextPhase()
+				}
+				return nil
+			}
+		}
+	case Alldone:
+		{
+			reply.TaskType = ExitTask
+		}
+	}
+	return nil
+}
+
+func (c *Coordinator) getTaskStatus(taskId int) TaskStatus {
+	var ret TaskStatus
+	taskHolder := c.TaskHolder
+	taskInfo := taskHolder.Meta[taskId]
+	if taskInfo == nil { // 找不到这个task
+		fmt.Printf("The task %d is not in TaskHolder.\n", taskId)
+	} else {
+		ret = taskInfo.TaskStatus
+	}
+	return ret
+}
+
+func (c *Coordinator) setTaskStatus(taskId int) {
+	c.TaskHolder.Meta[taskId].TaskStatus = Operating
+}
+
+func (c *Coordinator) checkAllTaskDone() bool { // 检查是不是所有当前阶段的任务都完成了 这里的任务只有两种状态 一种是正在执行 一种是已经做完了 不可能有等待执行的任务
+	var (
+		taskMapDoneCnt      = 0
+		taskMapUnDoneCnt    = 0
+		taskReduceDoneCnt   = 0
+		taskReduceUnDoneCnt = 0
+	)
+	taskHolder := c.TaskHolder
+	for _, taskInfo := range taskHolder.Meta {
+		if taskInfo.TaskAddr.TaskType == MapTask {
+			if taskInfo.TaskStatus == Done {
+				taskMapDoneCnt++
+			} else {
+				taskMapUnDoneCnt++
+			}
+		} else if taskInfo.TaskAddr.TaskType == ReduceTask {
+			if taskInfo.TaskStatus == Done {
+				taskReduceDoneCnt++
+			} else {
+				taskReduceUnDoneCnt++
+			}
+		}
+	}
+
+	fmt.Printf("taskMapDoneCnt: %d, taskMapUnDoneCnt: %d, taskReduceDoneCnt: %d, taskReduceUnDoneCnt: %d.\n", taskMapDoneCnt, taskMapUnDoneCnt, taskReduceDoneCnt, taskReduceUnDoneCnt)
+	if (taskMapDoneCnt > 0 && taskMapUnDoneCnt == 0) && (taskReduceDoneCnt == 0 && taskReduceUnDoneCnt == 0) { // reduce任务还没有开始做
+		// map tasks all done
+		return true
+	} else if taskReduceDoneCnt > 0 && taskReduceUnDoneCnt == 0 { // reduce任务做完了
+		return true
+	}
+
+	return false
+}
+
+func (c *Coordinator) toNextPhase() {
+	switch c.DistPhase {
+	case MapPhase:
+		{
+			c.makeReduceTasks()
+			c.DistPhase = ReducePhase
+			break
+		}
+	case ReducePhase:
+		{
+			c.DistPhase = Alldone
+			break
+		}
+	case Alldone:
+		{
+			break
+		}
+	default:
+		{
+			panic("Undefined Phase!\n")
+		}
+	}
+}
+
+func (c *Coordinator) MarkFinished(args *Task, reply *Task) error {
+	taskId := args.TaskId
+	taskHolder := c.TaskHolder.Meta
+	taskInfo := taskHolder[taskId]
+	if taskInfo == nil {
+		fmt.Printf("The task %d is not exists.\n", taskId)
+	} else if taskInfo.TaskStatus == Operating {
+		taskInfo.TaskStatus = Done
+		fmt.Printf("The task %d is done.\n", taskId)
+	} else if taskInfo.TaskStatus == Done {
+		fmt.Printf("The task %d is done before.\n", taskId)
+	} else if taskInfo.TaskStatus == Waiting {
+		fmt.Printf("The task %d's status should not be here.\n", taskId)
+	}
+	return nil
+}
+
+func (c *Coordinator) makeReduceTasks() {
+	for i := 0; i < c.ReducerNum; i++ {
+		id := c.genTaskId()
+		task := Task{
+			TaskType:  ReduceTask,
+			TaskId:    id,
+			FileNames: selectReduceName(i), // 选择这个reduce task需要传入的临时文件的名称
+		}
+
+		c.TaskHolder.AcceptTask(&task) // 将task放进TaskHolder中
+		fmt.Printf("The coordinator makes a reduce task, which task id is %d\n", task.TaskId)
+		c.ReduceTaskChannel <- &task
+	}
+}
+
+func selectReduceName(reducerId int) []string {
+	var ret []string
+	path, _ := os.Getwd()
+	fileInfos, _ := ioutil.ReadDir(path)
+	for _, fileInfo := range fileInfos {
+		if strings.HasPrefix(fileInfo.Name(), "mr-tmp") && strings.HasSuffix(fileInfo.Name(), strconv.Itoa(reducerId)) {
+			ret = append(ret, fileInfo.Name())
+		}
+	}
+	return ret
 }
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,42 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+)
 
+// for sorting by key.
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
+type Task struct {
+	TaskType   TaskType // 表示当前这个任务是干什么的 可选的任务类型有 MapTask ReduceTask WaitingTask ExitTask
+	TaskId     int      // 当前任务的id 所有任务的TaskId都是不同的
+	ReducerNum int      // 做reduce任务的woker数量 在hash的时候要用到 需要用它来%
+	FileNames  []string // 对于map task来说 传入的就是一个文件的名字 但是对于reduce task来说 传入的是多个文件的名称 所以对于map task 来说 我们处理第一个下标的就好
+}
+
+type TaskType int
+
+// 四种task
+const (
+	MapTask     TaskType = iota // 表示这个worker要去做Map task
+	ReduceTask                  // 表示这个woker要去做Reduce task
+	WaitingTask                 // 表示需要做的任务都分配出去了 但是这些任务还没有完成 当前这个worker等待就好
+	ExitTask                    // 表示所有的任务都做完了 worker可以退出了
+)
 
 //
 // Map functions return a slice of KeyValue.
@@ -24,18 +56,156 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
 //
 // main/mrworker.go calls this function.
 //
-func Worker(mapf func(string, string) []KeyValue,
-	reducef func(string, []string) string) {
+func Worker(mapf func(string, string) []KeyValue, reducef func(string, []string) string) {
+	workingFlag := true
+	for workingFlag {
+		task := getTask() // 请求Coordinator分配一个任务
+		switch task.TaskType {
+		case MapTask:
+			{
+				doMapTask(mapf, &task) // 如果是maptask 那么这个woker就去做map task
+				callDone(&task)        // 做完了 需要将这个任务标记为做完了
+			}
+		case ReduceTask:
+			{
+				doReduceTask(reducef, &task)
+				callDone(&task)
+			}
+		case WaitingTask:
+			{
+				fmt.Printf("All tasks are in progress! Task %d is waiting.\n", task.TaskId)
+				time.Sleep(time.Second)
+			}
+		case ExitTask:
+			{
+				fmt.Printf("Task %d is terminated...\n", task.TaskId)
+				workingFlag = false
+			}
+		default:
+			{
+				panic("undefined TaskType!!!")
+			}
+		}
+	}
+}
 
-	// Your worker implementation here.
+// 首先明确map task是在做什么
+// 一个map task将一个传进来的文件分成ReducerNum个中间文件 这些中间文件里面的内容是一个一个的键值对 这个时候还没有排序
+// 比如现在传进来一个文件 我们用一个woker去做这个任务(这个woker的编号是1)在做map task 定义了10个ReducerNum 那么它就会生成下面这些文件
+// mr-tmp-0-0 mr-tmp-0-1 mr-tmp-0-2 ... mr-tmp-0-9
+// 下面的代码主要参考mrsequential.go
+func doMapTask(mapf func(string, string) []KeyValue, task *Task) {
+	filename := task.FileNames[0]
 
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+	file, err := os.Open(filename) // 对于map reduce来说 我们只关心FileSlice中的第一个file
+	if err != nil {
+		log.Fatalf("Cannot open %v", filename)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("Cannot read %v", filename)
+	}
+	file.Close()
 
+	// 下面对从filename中取出来的content做map操作
+	intermediate := mapf(filename, string(content))
+
+	reducerNum := task.ReducerNum
+
+	hashBuckets := make([][]KeyValue, reducerNum) // 构造一个hash桶 有reducerNum个 每个里面都是很多的KeyValue对
+
+	for _, kv := range intermediate {
+		hashBuckets[ihash(kv.Key)%reducerNum] = append(hashBuckets[ihash(kv.Key)%reducerNum], kv)
+	}
+
+	for i := 0; i < reducerNum; i++ {
+		oname := "mr-tmp-" + strconv.Itoa(task.TaskId) + "-" + strconv.Itoa(i)
+		ofile, _ := os.Create(oname)
+		enc := json.NewEncoder(ofile)
+		// 编码进这个文件中
+		for _, kv := range hashBuckets[i] {
+			enc.Encode(kv)
+		}
+		ofile.Close()
+	}
+}
+
+// 参考mrsequence.go编写该函数
+func doReduceTask(reducef func(string, []string) string, task *Task) {
+	fileNames := task.FileNames
+	intermediate := sortContent(fileNames) // 返回的是排好序的键值对
+	dir, _ := os.Getwd()
+	tempFile, err := ioutil.TempFile(dir, "mr-tmp-*")
+	if err != nil {
+		log.Fatal("Failed to create temp file", err)
+	}
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+
+		// this is the correct format for each line of Reduce output.
+		fmt.Fprintf(tempFile, "%v %v\n", intermediate[i].Key, output)
+		i = j
+	}
+	tempFile.Close()
+	fileName := fmt.Sprintf("mr-out-%d", task.TaskId)
+	os.Rename(tempFile.Name(), fileName)
+}
+
+func sortContent(fileNames []string) []KeyValue {
+	var ret []KeyValue
+	for _, fileName := range fileNames {
+		file, _ := os.Open(fileName)
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			ret = append(ret, kv)
+		}
+		file.Close()
+	}
+	sort.Sort(ByKey(ret))
+	return ret
+}
+
+// call RPC将这个任务标记为做完了
+func callDone(task *Task) {
+	args := task
+	reply := Task{}
+
+	ok := call("Coordinator.MarkFinished", &args, &reply)
+
+	if ok {
+		fmt.Printf("Task %d finished.\n", task.TaskId)
+	} else {
+		fmt.Printf("Call failed in callDone\n")
+	}
+}
+
+func getTask() Task {
+	args := ExampleArgs{}
+	reply := Task{}
+
+	ok := call("Coordinator.AssignTask", &args, &reply)
+	if ok {
+		fmt.Printf("Coordinator assigned a task, and it is a %d task\n", reply.TaskType)
+	} else {
+		fmt.Printf("call failed!\n")
+	}
+	return reply
 }
 
 //
